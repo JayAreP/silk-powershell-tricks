54973602121b6616864964e8176452e6b144d4f3
JayAreP <43276115+JayAreP@users.noreply.github.com>
2021-05-19 12:31:57 -0700

    Update Create-SilkImage.ps1

--- Create-SilkImage.ps1
+++ Create-SilkImage.ps1
@@ -1,16 +1,58 @@
 param(
-    [parameter(Mandatory)]
+    [parameter()]
     [string] $target_rg,
     [parameter()]
     [string] $storage_container = 'silk',
-    [parameter()]
-    [string] $imageFileName = 'k2c-cnode-8-0-21-10.vhd',
+    [parameter(Mandatory)]
+    [string] $imageFileName,
     [parameter(Mandatory)]
     [string] $sasToken
 )
 
+function Build-MenuFromArray {
+    param(
+        [Parameter(Mandatory)]
+        [array]$array,
+        [Parameter(Mandatory)]
+        [string]$property,
+        [Parameter()]
+        [string]$message = "Select item"
+    )
+
+    Write-Host '------'
+    $menuarray = @()
+        foreach ($i in $array) {
+            $o = New-Object psobject
+            $o | Add-Member -MemberType NoteProperty -Name $property -Value $i.$property
+            $menuarray += $o
+        }
+    $menu = @{}
+    for (
+        $i=1
+        $i -le $menuarray.count
+        $i++
+    ) { Write-Host "$i. $($menuarray[$i-1].$property)" 
+        $menu.Add($i,($menuarray[$i-1].$property))
+    }
+    Write-Host '------'
+    [int]$mntselect = Read-Host $message
+    $menu.Item($mntselect)
+    Write-Host `n`n
+}
+
+if (!$target_rg) {
+    $rglist = Get-AzResourceGroup
+    $target_rg = Build-MenuFromArray -array $rglist -property resourcegroupname -message "Select the appropriate Resource Group"
+}
+
+
+$rg = Get-AzResourceGroup -Name $target_rg -ErrorAction SilentlyContinue
+if (!$rg) {
+    Write-Host -ForegroundColor yellow "Resource group -- $target_rg -- Not found. Please check and try again."
+    exit
+}
+
 $storage_accountname = 'images' + ( -join ((48..57) + (65..90) + (97..122) | Get-Random -Count 10 | ForEach-Object {[char]$_})).ToLower()
-$rg = Get-AzResourceGroup -Name $target_rg
 $imageName = $imageFileName.replace('.vhd',$null)
 
 # Create target storage account
@@ -22,7 +64,7 @@ $sc = $sa | New-AzStorageContainer -Name $storage_container
 Write-Host -ForegroundColor yellow "Copying $imageFileName to $storage_accountname"
 $sakeys = $sa | Get-AzStorageAccountKey
 
-$srcContext = New-AzStorageContext -StorageAccountName silkimages -SasToken $sasToken
+$srcContext = New-AzStorageContext -StorageAccountName silkimages -SasToken $sasToken 
 $dstContext = New-AzStorageContext -StorageAccountName $sa.StorageAccountName -StorageAccountKey $sakeys[0].Value
 Start-AzStorageBlobCopy -DestContainer $storage_container -SrcBlob $imageFileName -SrcContainer 'images' -DestContext $dstContext -Context $srcContext -DestBlob $imageFileName -Verbose 
 
